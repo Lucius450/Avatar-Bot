910e491bab43af5f8a8b1ecf98773f78b44b1389
Lucius450 <62286562+Lucius450@users.noreply.github.com>
2020-03-17 17:09:31 +0100

    Added a Discord Avatar Bot

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Avatar-Bot
 A Discord Bot, that sends a picture of your avatar.
+
+# Installation
+You must create a file named `auth.json` containing the following:  
+```json
+{
+    "token": "YOUR_DISCORD_TOKEN"
+}
+```
+To run, type `node bot.js`.
---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const { token } = require('./auth.json')
+//Import the Discord.js library and auth.json
+
+const client = new _client();
+
+client.on('ready', () => {
+    console.log('Started');
+});
+
+client.on('message', message => {
+    if (message.content === 'avatar') {
+        const embed = new MessageEmbed()
+        .setTitle('Avatar')
+        .setThumbnail(message.author.avatarURL())
+        .setDescription(message.member.toString() + ('\s Avatar'));
+
+        return message.channel.send('embed')
+
+    }
+})
+client.login(token)
